00d5c9e4b2559f4780138d90a7e1b215932e9d47
junya-code <ash0030ash@gmail.com>
2026-04-29 08:21:41 +0900

    コメント修正

--- src/main/java/com/example/bbs/security/LoginUser.java
+++ src/main/java/com/example/bbs/security/LoginUser.java
@@ -14,6 +14,9 @@ public class LoginUser extends User {
     // LoginUser は Spring Security の User を継承しているので、
     // アプリ側の User と名前が衝突する。
     // だからフルパス指定は必要。
+
+    // extends は “継承” を意味するが、コンストラクタの Collection<? extends GrantedAuthority>
+    // は “継承ではなく、型の許容範囲を広げるためのジェネリクス表現”
     public LoginUser(com.example.bbs.model.User user,
             Collection<? extends GrantedAuthority> authorities) {
         super(user.getUsername(), user.getPassword(), authorities);
