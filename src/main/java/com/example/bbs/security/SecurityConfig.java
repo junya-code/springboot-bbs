37581de8f2a68199c65aac461c64402b7479c7c6
junya-code <ash0030ash@gmail.com>
2026-07-11 07:53:04 +0900

    sitemap.xml作成

--- src/main/java/com/example/bbs/security/SecurityConfig.java
+++ src/main/java/com/example/bbs/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                                                 .requestMatchers("/", "/auth/register", "/contact/**",
                                                                 "/posts/privacy", "/posts/terms",
                                                                 "/css/**", "/js/**", "/images/**", "/favicon.ico",
-                                                                "/error", "/robots.txt")
+                                                                "/error", "/robots.txt", "/sitemap.xml")
                                                 .permitAll()
                                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                                 .anyRequest().authenticated())
